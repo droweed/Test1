2c0f7675381d1a2b1a826946e61c721bcd83b85f
Janelyn Ramos <janelyn@Janelyns-MacBook-Pro.local>
2014-09-20 20:23:46 +0800

    Initial Commit

---
+++ Test2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test2
+//
+//  Created by Janelyn Ramos on 9/20/14.
+//  Copyright (c) 2014 NA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test2
+//
+//  Created by Janelyn Ramos on 9/20/14.
+//  Copyright (c) 2014 NA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
